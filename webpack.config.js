f5b97c699e93607a3dbe0bfce19bfccb0793d2fe
jiyang <443239423@qq.com>
2022-10-27 16:26:09 +0800

    更新打包umd

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const path = require('path')
 module.exports = {
-    mode: 'development', // 注明生产环境下
+    mode: 'production', // 注明生产环境下
     entry: './src/index.js', // 指定入口文件
     output: {
         path: path.resolve(__dirname, 'lib'),
         filename: 'index.js',
+        libraryTarget: 'umd'
     },
+    devtool: 'source-map',
     module: {
         rules: [
             {
